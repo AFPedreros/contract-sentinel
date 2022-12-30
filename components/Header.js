88bdb3669cb3ecebbad97f8211129f89d6f4133a
AFPedreros <felipe.pedreros94@gmail.com>
2022-12-30 17:19:21 -0500

    make the navbar responsive

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,77 @@
 import Link from "next/link";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+    const [toggle, setToggle] = useState(false);
+
+    const toggleMenu = () => {
+        setToggle((prev) => !prev);
+    };
+
     return (
         <header
-            className=" sticky top-0 py-10 flex items-start justify-between max-w-4xl mx-auto
-        z-20 xl:items-center"
+            className="sticky top-0 py-10 px-6 flex justify-between max-w-4xl mx-auto
+        z-20 items-center"
         >
-            <Link href="/">Contract Sentinel</Link>
-            <div className=" flex gap-10">
-                <Link href="/audit-contract">Audit</Link>
-                <Link href="/contact-us">Contact</Link>
-                <Link href="/about-us">About</Link>
+            <Link className="font-bold hover:text-cyan-500" href="/">
+                Contract Sentinel
+            </Link>
+            <div className="hidden md:gap-10 md:flex">
+                <Link
+                    className="border-b border-white hover:text-cyan-500 hover:border-solid
+                    hover:border-b hover:border-cyan-500"
+                    href="/audit-contract"
+                >
+                    Audit
+                </Link>
+                <Link
+                    className="border-b border-white hover:text-cyan-500 hover:border-solid
+                    hover:border-b hover:border-cyan-500"
+                    href="/contact-us"
+                >
+                    Contact
+                </Link>
+                <Link
+                    className="border-b border-white hover:text-cyan-500 hover:border-solid
+                    hover:border-b hover:border-cyan-500"
+                    href="/about-us"
+                >
+                    About
+                </Link>
             </div>
+            <button onClick={toggleMenu} className="block md:hidden">
+                {toggle ? (
+                    <FontAwesomeIcon icon={faX} size="xl" />
+                ) : (
+                    <FontAwesomeIcon icon={faBars} size="xl" />
+                )}
+            </button>
+            {toggle ? (
+                <div className="md:hidden text-center absolute flex flex-col top-20 right-0 h-screen w-full bg-white">
+                    <Link
+                        className="border-b border-white hover:text-cyan-500"
+                        href="/audit-contract"
+                    >
+                        Audit
+                    </Link>
+                    <Link
+                        className="border-b border-white hover:text-cyan-500"
+                        href="/contact-us"
+                    >
+                        Contact
+                    </Link>
+                    <Link
+                        className="border-b border-white hover:text-cyan-500"
+                        href="/about-us"
+                    >
+                        About
+                    </Link>
+                </div>
+            ) : (
+                <></>
+            )}
         </header>
     );
 };
--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,11 @@ const Home = () => {
                 <title>Contract Sentinel</title>
             </Head>
             <Header />
-            <div className=" flex flex-col justify-center text-center max-w-4xl m-auto">
-                <div className=" pt-28 text-5xl">
+            <div className="flex flex-col justify-center px-6 text-center max-w-4xl m-auto">
+                <div className="text-4xl mt-10 md:mt-28 md:text-5xl">
                     <h1>Audit your smart contracts easy, cheap and fast.</h1>
                 </div>
-                <div className="header-subtitle pt-10 text-xl">
+                <div className="text-lg mt-10 md:text-xl">
                     <h2>
                         Find errors an vulnerabilities in smart contracts using
                         this AI-powered tool.
@@ -22,8 +22,8 @@ const Home = () => {
                 </div>
 
                 <Link
-                    className=" bg-black rounded-2xl w-fit m-auto mt-10 px-10 py-4 text-white
-                    text-xl font-semibold tracking-wide"
+                    className=" bg-black text-md rounded-2xl w-fit m-auto mt-10 px-10 py-4 text-white
+                    font-semibold tracking-wide md:text-xl"
                     href="/audit-contract"
                 >
                     Audit Smart Contract
