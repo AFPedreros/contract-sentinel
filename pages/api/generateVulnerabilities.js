9e2a8585eaef666b8b8dadc66fead0d05a129669
AFPedreros <felipe.pedreros94@gmail.com>
2023-01-06 08:59:20 -0500

    improve the prompts

--- pages/api/generateVulnerabilities.js
+++ pages/api/generateVulnerabilities.js
@@ -5,16 +5,18 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-
-const basePromptPrefix = `Check if this is a smart contract, if is not a smart contract respond "Please enter a smart contract". If is a smart contract respond "Yes this is a smart contract":
-`;
 const generateAction = async (req, res) => {
     // Run first prompt
 
+    const basePromptPrefix = `Check if this is a Ethereum solidity smart contract with his constructor:
+    ${req.body.userInput}
+    If is not a Ethereum solidity smart contract with his constructor respond only the words: "Please enter a smart contract.".
+    If is a smart contract respond "Yes this is a smart contract".
+    `;
+
     let baseCompletion = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: `${basePromptPrefix}
-        ${req.body.userInput}`,
+        prompt: `${basePromptPrefix}`,
         temperature: 0.5,
         max_tokens: 700,
     });
@@ -22,35 +24,22 @@ const generateAction = async (req, res) => {
     let basePromptOutput = baseCompletion.data.choices.pop();
 
     if (basePromptOutput.text.toLowerCase().includes("yes")) {
-        const secondPrompt = `Please analyze the smart contract and identify any vulnerabilities it may have. Also, provide a brief explanation of each vulnerability if possible. Follow the next template for each vulnerability:
+        const secondPrompt = `Please analyze this smart contract:
+        ${req.body.userInput}
+        Identify any vulnerabilities the smart contract may have. Also, provide a brief explanation of each vulnerability if possible. Follow the next template for each vulnerability:
         Vulnerability (number): (name of the vulnerability)
         (explanation of the vulnerability)
+        If the smart contract doesn't have vulnerabilities respond "This contract is secure.".
         `;
 
         baseCompletion = await openai.createCompletion({
             model: "text-davinci-003",
-            prompt: `${secondPrompt}
-            ${req.body.userInput}`,
+            prompt: `${secondPrompt}`,
             temperature: 1,
             max_tokens: 700,
         });
 
         basePromptOutput = baseCompletion.data.choices.pop();
-
-        // const thirdPrompt = `Please analyze the smart contract and provide recommendations to improve the vulnerabilities in the list ${basePromptOutput}. Follow the next template for each recommendation:
-        // Recommendation (number): (explanation of the recommendation)
-        // code example to fix the lines of the vulnerability
-        // `;
-
-        // baseCompletion = await openai.createCompletion({
-        //     model: "text-davinci-003",
-        //     prompt: `${thirdPrompt}
-        //     ${req.body.userInput}`,
-        //     temperature: 1,
-        //     max_tokens: 700,
-        // });
-
-        // basePromptOutput = baseCompletion.data.choices.pop();
     }
 
     res.status(200).json({
