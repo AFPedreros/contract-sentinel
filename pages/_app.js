a338159d5cfc8eb31f7699d049e002caaef55ada
AFPedreros <felipe.pedreros94@gmail.com>
2023-01-10 10:34:09 -0500

    fix GA

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,9 @@ import { Analytics } from "@vercel/analytics/react";
 import "./styles.css";
 
 import ReactGA from "react-ga";
+import Script from "next/script";
 
-const { GA_MEASUREMENT_ID } = process.env;
+const GA_MEASUREMENT_ID = "G-EKG0Q9HPVR";
 ReactGA.initialize(GA_MEASUREMENT_ID);
 
 export default function App({ Component, pageProps }) {
@@ -11,6 +12,20 @@ export default function App({ Component, pageProps }) {
         <>
             <Component {...pageProps} />
             <Analytics />
+
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+            />
+
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GA_MEASUREMENT_ID}');
+    `}
+            </Script>
         </>
     );
 }
