8737595ce772eb8cf5cb5992e38a0cde8bb943df
AFPedreros <felipe.pedreros94@gmail.com>
2023-01-10 08:41:55 -0500

    fix GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import { Analytics } from "@vercel/analytics/react";
 import "./styles.css";
 
+import ReactGA from "react-ga";
+
+const { GA_MEASUREMENT_ID } = process.env;
+ReactGA.initialize(GA_MEASUREMENT_ID);
+
 export default function App({ Component, pageProps }) {
     return (
         <>
