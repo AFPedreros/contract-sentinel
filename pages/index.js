a095d4dda3ac584ea94c7a0dad116fbab7f8edef
AFPedreros <felipe.pedreros94@gmail.com>
2023-01-22 01:47:57 -0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -14,17 +14,17 @@ export default function Home() {
             <div className='absolute top-0 left-0 z-20 w-full'>
                 <Header />
             </div>
-            <div className='flex h-screen w-full bg-[url(../assets/bg.png)] bg-cover px-6 text-center'>
-                <div className='md:w-3/5 flex flex-col items-center w-full mx-auto my-auto text-center'>
-                    <h1 className='md:max-w-3xl md:text-5xl md:leading-snug mx-auto text-4xl font-bold text-white'>
+            <div className='flex h-screen w-full bg-[url("../assets/bg.png")] bg-cover px-6 text-center'>
+                <div className='mx-auto my-auto flex w-full flex-col items-center text-center md:w-3/5'>
+                    <h1 className='mx-auto text-4xl font-bold text-white md:max-w-3xl md:text-5xl md:leading-snug'>
                         Audit your smart contracts easy and
                         <span className='text-indigo-600'> fast</span>
                     </h1>
-                    <p className='w-fit my-2 font-semibold'>
+                    <p className='my-2 w-fit font-semibold'>
                         Find errors and vulnerabilities in smart contracts using
                         this AI-powered tool
                     </p>
-                    <div className='flex gap-2 mt-6'>
+                    <div className='mt-6 flex gap-2'>
                         <Link
                             className='mr-3 hidden rounded-lg bg-indigo-600 px-5 py-2.5 text-center text-sm font-bold text-white focus:outline-none focus:ring-4 focus:ring-gray-800 hover:bg-indigo-900 md:mr-0 md:block'
                             href='/sentinel'
@@ -40,13 +40,13 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <div className='bg-slate-100 flex w-full h-screen px-6 text-center text-black'>
-                <div className='md:w-3/5 flex flex-col items-center w-full mx-auto my-auto text-center'>
-                    <h2 className='md:max-w-3xl md:text-5xl md:leading-snug text-4xl font-bold'>
+            <div className='flex h-screen w-full bg-slate-100 px-6 text-center text-black'>
+                <div className='mx-auto my-auto flex w-full flex-col items-center text-center md:w-3/5'>
+                    <h2 className='text-4xl font-bold md:max-w-3xl md:text-5xl md:leading-snug'>
                         <span className='text-indigo-600'>Empowering </span>
                         the Future: AI and Smart Contract
                     </h2>
-                    <div className='md:max-w-xl mx-auto'>
+                    <div className='mx-auto md:max-w-xl'>
                         <Image
                             className='mx-auto'
                             src={bookImage}
@@ -54,7 +54,7 @@ export default function Home() {
                             width={300}
                         />
                     </div>
-                    <p className='w-fit my-2 font-semibold'>
+                    <p className='my-2 w-fit font-semibold'>
                         Unlock the power of AI and Smart Contracts to pave the
                         way for a more secure, trusted, and decentralized future
                         where power is placed in the hands of the people, and
@@ -70,13 +70,13 @@ export default function Home() {
                 </div>
             </div>
             <div className='flex h-screen w-full items-center bg-[url(../assets/bg.png)] bg-cover px-6 text-center'>
-                <div className='md:w-1/2 w-full mx-auto'>
-                    <div className='md:max-w-xl mx-auto'>
-                        <h2 className='text-start md:max-w-3xl md:text-5xl md:leading-snug text-4xl font-bold'>
+                <div className='mx-auto w-full md:w-1/2'>
+                    <div className='mx-auto md:max-w-xl'>
+                        <h2 className='text-start text-4xl font-bold md:max-w-3xl md:text-5xl md:leading-snug'>
                             Get your smart contract
                             <label className='text-indigo-600'> audited</label>
                         </h2>
-                        <p className='w-fit text-start my-6'>
+                        <p className='my-6 w-fit text-start'>
                             Simply enter your smart contract source code and our
                             app will quickly scan for any vulnerabilities. In
                             addition to identifying potential issues, our app
@@ -93,8 +93,8 @@ export default function Home() {
                         </Link>
                     </div>
                 </div>
-                <div className='md:block md:w-1/2 hidden w-full mx-auto my-auto'>
-                    <div className='md:max-w-xl mx-auto'>
+                <div className='mx-auto my-auto hidden w-full md:block md:w-1/2'>
+                    <div className='mx-auto md:max-w-xl'>
                         <Image
                             className='mx-auto'
                             src={eyeImage}
